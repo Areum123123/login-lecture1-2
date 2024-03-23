a8b949140c9ff0595e575b9efcab2ded8bcf4855
areum <kang10232@naver.com>
2024-03-23 10:19:37 +0900

    ing

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,17 +1,23 @@
 'use strict';
 
-const hello = (req, res) => {
-    res.render("home/index");
-  }
+const output = {
+   home :(req, res) => {
+      res.render("home/index");
+    },
+   login : (req, res) =>{
+      res.render("home/login");
+   },
+};
 
-  const login = (req, res) =>{
-    res.render("home/login");
- }
+const process = {
+   login: (req, res) => {
+      console.log(req.body);
+   }
+}
+
 
  module.exports = {
-    hello,
-    login,
+    output,
+    process,
  }
- //function login(){} 와 같다고 생각하면 된다.
- //오브젝트는 {key: value} 인데 키값만적어주면 {key :key} 와같다.
- //{hello : hello}. {login :login} 이렇게 되는거란다 뭔말이여/ 
+ 
