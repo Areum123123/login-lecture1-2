f4228ade81ba5fd0d8ee9d40e7de4c7cf77ee189
areum <kang10232@naver.com>
2024-03-25 23:06:12 +0900

    UserStorage

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,42 +1,44 @@
 'use strict';
 
+const UserStorage = require("../../models/UserStorage");
+
+
 const output = {
-   home :(req, res) => {
+   home: (req, res) => {
       res.render("home/index");
-    },
-   login : (req, res) =>{
+   },
+   login: (req, res) => {
       res.render("home/login");
    },
 };
 
-const users ={
-   id: ["woorimIT", "나개발", "김팀장"],
-   psword: ["1234", "1234", "123456"],
-};
+
 
 const process = {
    login: (req, res) => {
-      const id =req.body.id,
+      const id = req.body.id,
          psword = req.body.psword;
 
-         if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword){
-               return res.json({
-                  success : true,
-               });
-            }
+    
+     const users = UserStorage.getUsers("id", "psword");
+
+      const response = {};
+      if (users.id.includes(id)) {
+         const idx = users.id.indexOf(id);
+         if (users.psword[idx] === psword) {
+            response.success = true;
+            return res.json(response);
          }
-       return res.json({
-         success: false,
-         msg: "로그인에 실패하셨습니다.",
-       });
+      }
+
+      response.success = false;
+      response.msg = "로그인에 실패 하셨습니다."
+      return res.json(response);
    },
 };
 
 
- module.exports = {
-    output,
-    process,
- }
- 
+module.exports = {
+   output,
+   process,
+}
